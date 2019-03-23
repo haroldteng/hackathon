2414ff26864ced7bfbc70416b7027f835db16bb1
benjamin loh <benjaminlohwenqian@gmail.com>
2019-03-24 07:12:21 +0800

    PUSH

--- Hackathon/app/src/main/java/com/example/hackathon/HomeFragment.java
+++ Hackathon/app/src/main/java/com/example/hackathon/HomeFragment.java
@@ -1,72 +1,85 @@
 package com.example.hackathon;
 
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-import static android.support.constraint.Constraints.TAG;
+//import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeFragment extends Fragment {
+
+    private RecyclerView blog_list_view;
+    private List<BlogPost> blog_list;
+
+    private FirebaseFirestore firebaseFirestore;
+   // private FirebaseAuth firebaseAuth;
+    private BlogRecyclerAdapter blogRecyclerAdapter;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    View v = inflater.inflate(R.layout.fragment_home, container, false);
-       Button mSendData =(Button) v.findViewById(R.id.sendData);
-        mSendData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseApp.initializeApp(getContext());
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
-                FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-                        .setTimestampsInSnapshotsEnabled(true)
-                        .build();
-                db.setFirestoreSettings(settings);
-                Map<String, Object> user = new HashMap<>();
-                user.put("first", "Ada");
-                user.put("last", "Lovelace");
-                user.put("born", 1815);
 
-// Add a new document with a generated ID
-                db.collection("users")
-                        .add(user)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w(TAG, "Error adding document", e);
-                            }
-                        });
-            }
-        });
-        return v;
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        blog_list = new ArrayList<>();
+        blog_list_view = view.findViewById(R.id.blog_list_view);
+
+       // firebaseAuth = FirebaseAuth.getInstance();
+
+        blogRecyclerAdapter = new BlogRecyclerAdapter(blog_list);
+        blog_list_view.setLayoutManager(new LinearLayoutManager(container.getContext()));
+        blog_list_view.setAdapter(blogRecyclerAdapter);
+
+
+        //if (firebaseAuth.getCurrentUser() != null){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+        db.collection("Shops").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    for(QueryDocumentSnapshot document:task.getResult()) {
+                        Log.d("Debug", document.getString("desc"));
+
+                        BlogPost blogPost = document.toObject(BlogPost.class);
+                        blog_list.add(blogPost);
+                        blogRecyclerAdapter.notifyDataSetChanged();
+                    }}
+            else{
+                    Log.d("DEBUG", "Error getting documents: ", task.getException());
+                }
+            }});
+
+        //}
+
+        return view;
+
+
+
+
+
+
 
     }
+
+
 
 }
---
+++ Hackathon2019/app/src/main/res/layout/HomeFragment.java
@@ -0,0 +1,68 @@
+package com.example.hackathon;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class HomeFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    View v = inflater.inflate(R.layout.fragment_home, container, false);
+       Button mSendData =(Button) v.findViewById(R.id.sendData);
+        mSendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseApp.initializeApp(getContext());
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                        .setTimestampsInSnapshotsEnabled(true)
+                        .build();
+                db.setFirestoreSettings(settings);
+                Map<String, Object> user = new HashMap<>();
+                user.put("first", "Ada");
+                user.put("last", "Lovelace");
+                user.put("born", 1815);
+
+// Add a new document with a generated ID
+                db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+            }
+        });
+        return v;
+
+    }
+
+}
